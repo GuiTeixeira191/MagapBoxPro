de809dfcde1af484cc0a95b8c986fc735e1f998d
Magap <guilhermeart2020@outlook.com>
2022-12-20 13:02:46 -0300

    footer updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c049566c5e5e4428"; }
+/******/ 		__webpack_require__.h = function() { return "6119ecbef1d442c3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,44 +1,50 @@
 import React from 'react'
 import style from './Footer.module.css'
+import Image from 'next/image';
 import { BsLinkedin, BsGithub, BsYoutube, BsInstagram, BsFacebook } from "react-icons/bs";
 
 const Footer = () => {
     return (
         <div className={style.footer}>
+            <div className={style.logo}>
+                <Image src='/logo_magap.png' width={100} height={100} alt="Logo" />
+            </div>
             <div className={style.footer_container}>
+
                 <div className={style.media_container}>
                     <a href="https://www.linkedin.com/">
-                    <div className={style.media} >Linkedin&nbsp;<BsLinkedin/></div>
+                    <div className={style.media_l} >Linkedin&nbsp;<BsLinkedin/></div>
                     </a>
                 </div>
 
                 <div className={style.media_container}>
                     <a href="https://www.youtube.com/">
-                    <div className={style.media}>Youtube&nbsp; <BsYoutube/></div>
+                    <div className={style.media_y}>Youtube&nbsp; <BsYoutube/></div>
                     </a>
                 </div>
 
                 <div className={style.media_container}>
                     <a href="https://instagram.com/">
-                    <div className={style.media}>Instagram&nbsp;<BsInstagram /></div>
+                    <div className={style.media_i}>Instagram&nbsp;<BsInstagram /></div>
                     </a>
                 </div>
 
                 <div className={style.media_container}>
                     <a href="https://www.facebook.com/">
-                    <div className={style.media}>Facebook&nbsp;<BsFacebook/></div>
+                    <div className={style.media_f}>Facebook&nbsp;<BsFacebook/></div>
                     </a>
                 </div>
             </div>
                 <div className={style.me}>
-                    Projeto desenvolvido por: Guilherme Teixeira /&nbsp;
+                    Projeto desenvolvido por: Guilherme Teixeira
                     <a  href= 'https://github.com/GuiTeixeira191' >
-                    <BsGithub/>&nbsp;<BsLinkedin/>&nbsp;<BsFacebook/>
-                    </a>
+                        <BsGithub/> &nbsp; <BsLinkedin/> &nbsp; <BsFacebook/>
+                    </a> 
+                    <small className={style.small}>
+                        <p> &copy; All Rights Reserved</p>
+                    </small>
                 </div>
-                <small className='container'>
-                    <p> &copy; All Rights Reserved</p>
-                </small>
+
             
         </div>
     )
